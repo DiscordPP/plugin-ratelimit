027cc3c115dda9d2f7ab16dfcbcd4d285cf8767e
Aidan Edwards <Aidan.S.Edwards@gmail.com>
2020-05-20 14:41:54 -0500

    Attempt to defer calls

--- discordpp/plugin-ratelimit.hh
+++ discordpp/plugin-ratelimit.hh
@@ -4,5 +4,16 @@ namespace discordpp{
 	template<class BASE>
 	class PluginRateLimit: public BASE, virtual BotStruct{
 	public:
+		void call(
+				sptr<const std::string> requestType,
+				sptr<const std::string> targetURL,
+				sptr<const json> body,
+				sptr<const std::function<void(const json)>> callback
+		) override{
+			std::cout << "here" << std::endl;
+			BASE::call(requestType, targetURL, body, [callback](const json &msg){
+				(*callback)(msg);
+			});
+		}
 	};
 }
