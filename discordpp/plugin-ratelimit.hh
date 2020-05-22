ba5909356cc65f58cad5e5df406f351ad6f2e259
Aidan Edwards <Aidan.S.Edwards@gmail.com>
2020-05-21 21:46:43 -0500

    intercept `onWrite` and `onRead` of the `call`

--- discordpp/plugin-ratelimit.hh
+++ discordpp/plugin-ratelimit.hh
@@ -4,16 +4,33 @@ namespace discordpp{
 	template<class BASE>
 	class PluginRateLimit: public BASE, virtual BotStruct{
 	public:
-		void call(
+		virtual void call(
 				sptr<const std::string> requestType,
 				sptr<const std::string> targetURL,
 				sptr<const json> body,
-				sptr<const std::function<void(const json)>> callback
+				sptr<const std::function<void()>> on_write,
+				sptr<const std::function<void(const json)>> on_read
 		) override{
-			std::cout << "here" << std::endl;
-			BASE::call(requestType, targetURL, body, [callback](const json &msg){
-				(*callback)(msg);
-			});
+			auto id = ids.find(*targetURL);
+			BASE::call(
+					requestType, targetURL, body,
+					std::make_shared<std::function<void()>>(
+							[on_write](){
+								if(on_write != nullptr){
+									(*on_write)();
+								}
+								std::cout << "on_write" << std::endl;
+							}
+					),
+					std::make_shared<std::function<void(const json)>>(
+							[on_read](const json &msg){
+								std::cout << "on_read" << std::endl;
+								if(on_read != nullptr){
+									(*on_read)(msg);
+								}
+							}
+					)
+			);
 		}
 	};
 }
